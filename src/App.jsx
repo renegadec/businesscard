ee469c38d256a3c64641cc2349f359895d0382d6
renegadec <confidencenyirenda7@gmail.com>
2025-12-23 10:38:33 +0200

    added text and images and footer

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import MainComponent from './components/MainComponent'
+import Footer from './components/Footer'
+
+function App() {
+  
+
+  return (
+    <>
+      <MainComponent />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MainComponent.jsx
@@ -0,0 +1,24 @@
+export default function MainComponent() {
+    return (
+        <main className='main-component'>
+            <img src='/src/assets/me.png' alt='profile photo' className='profile-photo' />
+            <div>
+                <h1>Confidence Nyirenda</h1>
+                <p>Fullstack Developer</p>
+                <p>africoiner.com</p>
+            </div>
+            <div>
+                <button>Email</button>
+                <button>LinkedIn</button>
+            </div>
+            <div>
+                <h2>About</h2>
+                <p>I am a fullstack developer with a passion for building web applications that are both functional and aesthetically pleasing.</p>
+            </div>
+            <div>
+                <h2>Interests</h2>
+                <p>Web Development, AI, Machine Learning, Blockchain, Cryptocurrency, Golf, Traveling, Reading, Writing, and Cooking.</p>
+            </div>
+        </main>
+    )
+}
