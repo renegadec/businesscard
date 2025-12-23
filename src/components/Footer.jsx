d7498d5697b4f75d9a7d3c2147b7dfd29cc7d49e
renegadec <confidencenyirenda7@gmail.com>
2025-12-23 15:20:05 +0200

    styled main and footer components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,13 @@
+import { FaXTwitter, FaSquareFacebook, FaSquareInstagram, FaSquareGithub  } from "react-icons/fa6";
+
+
 export default function Footer() {
     return (
-        <h2>this is the footer</h2>
+        <div className="footer"> 
+            <FaXTwitter />
+            <FaSquareFacebook />
+            <FaSquareInstagram />
+            <FaSquareGithub />
+        </div>
     )
 }
--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,21 +1,28 @@
+import { IoMdMail } from "react-icons/io";
+import { GrLinkedin } from "react-icons/gr";
+
 export default function MainComponent() {
     return (
         <main className='main-component'>
             <img src='/src/assets/me.png' alt='profile photo' className='profile-photo' />
-            <div>
+            <div className='title-container'>
                 <h1>Confidence Nyirenda</h1>
                 <p>Fullstack Developer</p>
                 <p>africoiner.com</p>
             </div>
-            <div>
-                <button>Email</button>
-                <button>LinkedIn</button>
+            <div className='button-container'>
+                <button>
+                    <IoMdMail /> Email
+                </button>
+                <button>
+                   <GrLinkedin /> LinkedIn
+                </button>
             </div>
-            <div>
+            <div className='about-container'>
                 <h2>About</h2>
-                <p>I am a fullstack developer with a passion for building web applications that are both functional and aesthetically pleasing.</p>
-            </div>
-            <div>
+                <p>I am a fullstack developer with a passion for building web applications that are both functional and aesthetically pleasing. I have particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+            </div> 
+            <div className='interests-container'>
                 <h2>Interests</h2>
                 <p>Web Development, AI, Machine Learning, Blockchain, Cryptocurrency, Golf, Traveling, Reading, Writing, and Cooking.</p>
             </div>
